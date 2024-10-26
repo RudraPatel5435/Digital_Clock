bc7664c8bf509fa7bafd0da29ba57c52d55e40fc
RudraPatel5435 <rudranpatel0980@gmail.com>
2024-10-26 22:08:44 +0530

    Made the clock

--- script.js
+++ script.js
@@ -21,12 +21,49 @@ let months = {
     11: "November",
     12: "December"
 }
-let curr = new Date()
-const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-let day = curr.getDay()
-let week = weeks[curr.getDay()]
-let month = months[curr.getMonth()]
-let year = curr.getFullYear()
-let hour = curr.getHours()
-let min = curr.getMinutes()
-let sec = curr.getSeconds()
+setInterval(()=>{
+    let curr = new Date()
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    const day = curr.getDay()
+    const week = weeks[curr.getDay()]
+    const month = months[curr.getMonth()]
+    const year = curr.getFullYear()
+    const hour = curr.getHours()
+    const min = curr.getMinutes()
+    const sec = curr.getSeconds()
+    
+    var hours = document.querySelector("#hours")
+    var minutes = document.querySelector("#minutes")
+    var seconds = document.querySelector("#seconds")
+    var date = document.querySelector("#date")
+    var timeZonespan = document.querySelector("#timezone")
+    
+    hours.innerText = hour < 10 ? "0"+ hour : hour
+    minutes.innerText = min < 10 ? "0"+ min : min
+    seconds.innerText = sec < 10 ? "0"+ sec : sec
+    date.innerText = `${day} ${month} ${year}`
+    timeZonespan.innerText = timeZone
+
+}, 1000)
+
+let clock = document.querySelector(".clock")
+let font1 = document.querySelector(".font1")
+let font2 = document.querySelector(".font2")
+let font3 = document.querySelector(".font3")
+var timeall = document.getElementsByClassName("time")
+
+font1.addEventListener("click", () => {
+    date.style.fontFamily = "Digital Dream"
+    timeZonespan.style.fontFamily = "Digital Dream"
+    timeall.style.fontFamily = "Digital Dream"
+})
+font2.addEventListener("click", () => {
+    date.style.fontFamily = "Square Sans Serif 7"
+    timeZonespan.style.fontFamily = "Square Sans Serif"
+    timeall.style.fontFamily = "Square Sans Serif"
+})
+font2.addEventListener("click", () => {
+    date.style.fontFamily = "ClickClock"
+    timeZonespan.style.fontFamily = "ClickClock"
+    timeall.style.fontFamily = "ClickClock"
+})
