0ada792b3e861de205c38442b369a0f08e092579
RudraPatel5435 <rudranpatel0980@gmail.com>
2024-10-27 00:53:38 +0530

    Clock Finalized

--- script.js
+++ script.js
@@ -1,69 +1,50 @@
 let weeks = {
+    0: "Sunday",
     1: "Monday",
     2: "Tuesday",
     3: "Wednesday",
     4: "Thursday",
     5: "Friday",
-    6: "Saturday", 
-    7: "Sunday"
+    6: "Saturday"
 }
 let months = {
-    1: "January",
-    2: "Febuary",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December"
+    0: "January",
+    1: "Febuary",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December"
 }
+var hours = document.querySelector("#hours")
+var minutes = document.querySelector("#minutes")
+var seconds = document.querySelector("#seconds")
+var datespan = document.querySelector("#date")
+var timeZonespan = document.querySelector("#timezone")
+var dayspan = document.querySelector("#day")
 setInterval(()=>{
     let curr = new Date()
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    const day = curr.getDay()
+    const day = weeks[curr.getDay()]
     const week = weeks[curr.getDay()]
     const month = months[curr.getMonth()]
     const year = curr.getFullYear()
     const hour = curr.getHours()
     const min = curr.getMinutes()
     const sec = curr.getSeconds()
+    const date = curr.getDate()
     
-    var hours = document.querySelector("#hours")
-    var minutes = document.querySelector("#minutes")
-    var seconds = document.querySelector("#seconds")
-    var date = document.querySelector("#date")
-    var timeZonespan = document.querySelector("#timezone")
     
     hours.innerText = hour < 10 ? "0"+ hour : hour
     minutes.innerText = min < 10 ? "0"+ min : min
     seconds.innerText = sec < 10 ? "0"+ sec : sec
-    date.innerText = `${day} ${month} ${year}`
+    datespan.innerText = `${date} ${month} ${year}`
     timeZonespan.innerText = timeZone
+    dayspan.innerText = `${day}`
 
 }, 1000)
-
-let clock = document.querySelector(".clock")
-let font1 = document.querySelector(".font1")
-let font2 = document.querySelector(".font2")
-let font3 = document.querySelector(".font3")
-var timeall = document.getElementsByClassName("time")
-
-font1.addEventListener("click", () => {
-    date.style.fontFamily = "Digital Dream"
-    timeZonespan.style.fontFamily = "Digital Dream"
-    timeall.style.fontFamily = "Digital Dream"
-})
-font2.addEventListener("click", () => {
-    date.style.fontFamily = "Square Sans Serif 7"
-    timeZonespan.style.fontFamily = "Square Sans Serif"
-    timeall.style.fontFamily = "Square Sans Serif"
-})
-font2.addEventListener("click", () => {
-    date.style.fontFamily = "ClickClock"
-    timeZonespan.style.fontFamily = "ClickClock"
-    timeall.style.fontFamily = "ClickClock"
-})
